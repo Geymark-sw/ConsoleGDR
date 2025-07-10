ff9a70e78496c2f9cbdf1c364cdcd923d99d76c4
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2025-07-10 16:27:11 +0200

    aggiunte funzioni a randoroom

--- src/theRiseOfITS/concreto/rooms/RandomRoom.java
+++ src/theRiseOfITS/concreto/rooms/RandomRoom.java
@@ -1,12 +1,22 @@
 package theRiseOfITS.concreto.rooms;
 
+import java.util.List;
+
+import theRiseOfITS.astratto.Item;
 import theRiseOfITS.astratto.Room;
+import theRiseOfITS.concreto.entity.Mob;
+import theRiseOfITS.utilities.FactoryItem;
+import theRiseOfITS.utilities.FactoryMob;
 
 public class RandomRoom extends Room{
 	
+	private Mob mob;
+	private List<Item> loot;
 	
 	public RandomRoom(String name) {
 		super(name);
+		this.mob = FactoryMob.generateRandomMob();
+		this.loot = FactoryItem.generaDropCasuale(3);
 	}
 	
 	
