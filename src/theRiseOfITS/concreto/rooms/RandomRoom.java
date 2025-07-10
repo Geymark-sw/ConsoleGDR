38bddb4a0f1eefef2308b196415ed569f3381c4a
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2025-07-10 16:31:21 +0200

    aggiunte funzioni a randomroom

--- src/theRiseOfITS/concreto/rooms/RandomRoom.java
+++ src/theRiseOfITS/concreto/rooms/RandomRoom.java
@@ -18,6 +18,23 @@ public class RandomRoom extends Room{
 		this.mob = FactoryMob.generateRandomMob();
 		this.loot = FactoryItem.generaDropCasuale(3);
 	}
+
+	public Mob getMob() {
+		return mob;
+	}
+
+	public void setMob(Mob mob) {
+		this.mob = mob;
+	}
+
+	public List<Item> getLoot() {
+		return loot;
+	}
+
+	public void setLoot(List<Item> loot) {
+		this.loot = loot;
+	}
+	
 	
 	
 }
