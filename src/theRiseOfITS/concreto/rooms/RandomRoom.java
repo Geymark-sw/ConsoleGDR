b6a019b811b60243f98c7fbeaadcaf044f07746e
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2025-07-11 09:52:21 +0200

    aggiunte chest in randomroom

--- src/theRiseOfITS/concreto/rooms/RandomRoom.java
+++ src/theRiseOfITS/concreto/rooms/RandomRoom.java
@@ -1,20 +1,51 @@
 package theRiseOfITS.concreto.rooms;
 
 import java.util.List;
+import java.util.Random;
 
+import theRiseOfITS.astratto.Chest;
 import theRiseOfITS.astratto.Item;
 import theRiseOfITS.astratto.Room;
-import theRiseOfITS.concreto.entity.Mob;
+import theRiseOfITS.concreto.chests.StandardChest;
 import theRiseOfITS.utilities.FactoryItem;
 import theRiseOfITS.utilities.FactoryMob;
 
-public class RandomRoom extends Room{
+public class RandomRoom extends Room {
 
-		
+	private Chest chest;
+
 	public RandomRoom(String name) {
 		super(name);
 		this.setMobs(FactoryMob.getMobList());
 		this.setItems(FactoryItem.generaDropCasuale(3));
+		generaChest();
 	}
-	
+
+	private void generaChest() {
+		Random rand = new Random();
+		boolean haUnaChest = rand.nextBoolean(); // 50% di possibilità, come in goldchest
+
+		if (haUnaChest) {
+			List<Item> oggetti = FactoryItem.generaDropCasuale(5);
+			this.chest = new StandardChest("Chest comune", oggetti);
+		}
+	}
+
+	public boolean hasChest() {
+		return chest != null;
+	}
+
+	public Chest getChest() {
+		return chest;
+	}
+
+	public void setChest(Chest chest) {
+		this.chest = chest;
+	}
+
+	@Override
+	public String toString() {
+		return "RandomRoom [chest=" + chest + "]";
+	}
+
 }
